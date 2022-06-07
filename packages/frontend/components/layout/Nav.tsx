69cca19d1aed123eb67cf74427392f0e8db413b9
Joshua Healey (@Alpine-lines) <healey.josh@yahoo.com>
2022-06-07 15:36:38 -0600

    @uath/js, Context w/ Reducer

--- packages/frontend/components/layout/Nav.tsx
+++ packages/frontend/components/layout/Nav.tsx
@@ -2,7 +2,7 @@ import {
     Button,
     Container,
     Flex,
-    Image,
+    // Image,
     Link,
     Menu,
     MenuButton,
@@ -10,23 +10,56 @@ import {
     MenuList,
     SimpleGrid,
 } from '@chakra-ui/react'
-import { useEthers } from '@usedapp/core'
-import blockies from 'blockies-ts'
+import Client from '@uauth/js'
 import NextLink from 'next/link'
-import Balance from '../web3/Balance'
-import ConnectWallet from '../web3/ConnectWallet'
+import UAuthSPA from '@uauth/js'
+import { useEffect } from 'react'
+import { UserInfo } from '@uauth/js'
+import { useEthers } from '@usedapp/core'
+import web3modal from '../../lib/connectors'
 import { truncateHash } from '../../lib/utils'
+import { useAuth } from '../../lib/context/auth'
+import {uauthOptions} from '../../lib/connectors'
+import * as UAuthWeb3Modal from '@uauth/web3modal'
 
 function Nav() {
-    const { account, deactivate } = useEthers()
+    const { deactivate } = useEthers();
+    const { isLoading, walletAddress, client, login, authDispatch } = useAuth();
 
-    let blockieImageSrc
-    if (typeof window !== 'undefined') {
-        blockieImageSrc = blockies.create({ seed: account }).toDataURL()
-    }
+    useEffect(() => {
+      UAuthWeb3Modal.getUAuth(UAuthSPA, uauthOptions)
+        .loginCallback()
+        .then(async () => {
+          authDispatch({ type: 'setClient', value: await web3modal.connectTo('custom-uauth') });
+          authDispatch({ type: 'setIsLoading', value: true });
+          client
+            .user()
+            .then((user: UserInfo) => authDispatch({ type: 'loginAction', value: { walletAddress: user.wallet_address, user, isLoggedIn: true } }))
+            .catch((err: string) => {
+              throw new Error(err)
+            })
+            .finally(() => authDispatch({ type: 'setIsLoading', value: true }));
+        })
+        .catch(error => {
+          throw new Error(error); // TODO: Redirect to failure page
+        })
+    }, [])
+
+
+    const handleWeb3Modal = async () => {
+      const client: Client = await web3modal.connect();
+      authDispatch({ type: 'setClient', value: client })
+      login();
+      // await new UAuth(uauthOptions)
+      //   .user()
+      //   .then((user: UserInfo) => authDispatch({ type: 'setIsLoading', value: true }))
+      //   .catch(error => {
+      //     throw new Error(error); // TODO: Redirect to failure page
+      // });
+    };
     
     return (
-        <header>
+      <header>
         <Container maxWidth="container.xl">
           <SimpleGrid
             columns={[1, 1, 1, 2]}
@@ -51,17 +84,15 @@ function Nav() {
                 </Link>
               </NextLink>
             </Flex>
-            {account ? (
+            {walletAddress ? (
               <Flex
                 order={[-1, null, null, 2]}
                 alignItems={'center'}
                 justifyContent={['flex-start', null, null, 'flex-end']}
               >
-                <Balance />
-                <Image ml="4" src={blockieImageSrc} alt="blockie" />
                 <Menu placement="bottom-end">
                   <MenuButton as={Button} ml="4">
-                    {truncateHash(account)}
+                    {truncateHash(walletAddress)}
                   </MenuButton>
                   <MenuList>
                     <MenuItem onClick={deactivate}>Disconnect</MenuItem>
@@ -69,7 +100,14 @@ function Nav() {
                 </Menu>
               </Flex>
             ) : (
-              <ConnectWallet />
+
+              <Button
+              isLoading={isLoading}
+              colorScheme='teal'
+              variant='outline'
+              color="#0D67FE" onClick={handleWeb3Modal}>
+                Login With Unstoppable
+              </Button>
             )}
           </SimpleGrid>
         </Container>
