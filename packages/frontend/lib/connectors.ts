689c2f29112fc790ff7f8c722a818e49418d10a9
Joshua Healey (@Alpine-lines) <healey.josh@yahoo.com>
2022-06-15 06:58:22 -0600

    DonorRewardsNFT & HuntGovernorVotes bugfixes

--- packages/frontend/lib/connectors.ts
+++ packages/frontend/lib/connectors.ts
@@ -1,19 +1,39 @@
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
-import { WalletLinkConnector } from '@web3-react/walletlink-connector'
+import * as UAuthWeb3Modal from '@uauth/web3modal'
+import UAuthSPA from '@uauth/js'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import Web3Modal from 'web3modal'
 
-const POLLING_INTERVAL = 12000
-const RPC_URLS: { [chainId: number]: string } = {
-  1: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
-  4: 'https://rinkeby.infura.io/v3/84842078b09946638c03157f83405213',
+// These options are used to construct the UAuthSPA instance: IUAuthOptions
+export const uauthOptions: UAuthWeb3Modal.IUAuthOptions = {
+  clientID: 'hunter-dao-dapp',
+  redirectUri: 'http://localhost:3000',
+  scope: 'openid wallet',
 }
-export const walletconnect = new WalletConnectConnector({
-  rpc: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
-  qrcode: true,
-  pollingInterval: POLLING_INTERVAL,
-})
 
-export const walletlink = new WalletLinkConnector({
-  url: RPC_URLS[1],
-  appName: 'Coinbase',
-  supportedChainIds: [1, 3, 4, 5, 42, 10, 137, 69, 420, 80001],
-})
+const providerOptions = {
+  // Custom `web3modal` providers egistered using the "custom-" prefix.
+  'custom-uauth': {
+    display: UAuthWeb3Modal.display,
+
+    connector: UAuthWeb3Modal.connector,
+
+    package: UAuthSPA,
+
+    options: uauthOptions,
+  },
+
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: process.env.INFURA_ID,
+    },
+  },
+
+  // Include any other web3modal providers here too...
+}
+
+const web3modal = new Web3Modal({providerOptions})
+
+UAuthWeb3Modal.registerWeb3Modal(web3modal) // Register the web3modal...
+
+export default web3modal;
